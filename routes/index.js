ad71ae5612d84afdbb6db0bc657bee1b34d80d45
Reejh Ghosh <r4reejh@hotmail.com>
2017-03-03 12:04:03 +0530

    initial

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var passport= require('passport')
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.ejs');
+});
+
+/*router.get('/login', function(req, res, next) {
+  res.render('login.ejs', { message: req.flash('loginMessage') });
+});
+
+router.get('/signup', function(req, res) {
+  res.render('signup.ejs', { message: req.flash('loginMessage') });
+});*/
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.post('/signup', passport.authenticate('local-signup', {
+  successRedirect: '/profile',
+  failureRedirect: '/signup',
+  failureFlash: true,
+}));
+
+router.post('/login', passport.authenticate('local-login', {
+  successRedirect: '/profile',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+router.get('/profile',function(req,res){
+  //res.send("test");
+  res.send(req.user);
+});
+
+module.exports = router;
